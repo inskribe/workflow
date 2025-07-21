48fcd999a2a3b9d30a2898e4cf389386a7b62ca2
inskribe <inskribe.dev@tuta.com>
2025-07-21 16:52:21 -0400

    workflows

---
+++ cmd/some/some_test.go
@@ -0,0 +1,10 @@
+package some
+
+import "testing"
+
+func TestSome(t *testing.T) {
+	if (1 + 2) == 3 {
+		t.Log("math, maths")
+		return
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/inskribe/workflows
+
+go 1.24.5
